2c465ba84000db743577c4e4158559052f7e80a1
Régis Daum <contact@graphandco.com>
2025-12-24 23:33:52 +0100

    Add Topbar

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
    return (
-      <section className="hero container mx-auto">
-         <h1 className="text-4xl font-bold">Hello World !!!</h1>
+      <section className="hero wrapper">
+         <h1 className="text-4xl font-bold text-primary">Hello World !!!</h1>
          <p className="text-lg">This is a paragraph</p>
-         <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
+         <button className="bg-accent text-primary font-semibold px-4 py-2 rounded-md">
             Click me
          </button>
       </section>
---
+++ components/header/Topbar.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Phone, MapPin } from "lucide-react";
+export default function Topbar() {
+   return (
+      <div className="bg-black text-white">
+         <div className="wrapper py-2 flex gap-4 items-center ">
+            <Link
+               href="tel:0389454436"
+               className="flex items-center gap-1 cursor-pointer hover:text-accent"
+            >
+               <Phone size={14} className="text-accent" />
+               <span className="text-sm">03 89 45 44 36</span>
+            </Link>
+            <div className="flex items-center gap-1">
+               <MapPin size={14} className="text-accent" />
+               <span className="text-sm">
+                  7 rue des Alpes, 68127 Niederentzen
+               </span>
+            </div>
+         </div>
+      </div>
+   );
+}
