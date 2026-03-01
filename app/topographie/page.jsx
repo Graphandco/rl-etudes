0f32779f64f06f3c66a71cb0b0a66ca7ca723f5d
Régis Daum <contact@graphandco.com>
2026-03-01 11:15:02 +0100

    Topo quinconce

--- app/topographie/page.jsx
+++ app/topographie/page.jsx
@@ -1,10 +1,12 @@
 import { getWordpressContent } from "@/actions/getWordpressContent";
 import TopoHero from "@/components/topographie/TopoHero";
+import TopoPrestations from "@/components/topographie/TopoPrestations";
 export default async function Topographie() {
    const pageData = await getWordpressContent({ id: 28, type: "page" });
    return (
       <div>
          <TopoHero pageData={pageData} />
+         <TopoPrestations />
       </div>
    );
 }
